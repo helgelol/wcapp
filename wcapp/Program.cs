4c8046b69c4ffcc98e2a6e75c2b827a70c3c042c
masunile <helge@furion.se>
2019-04-18 21:35:33 +0200

    Rewritten in C# Missing error handling if not int

---
+++ wcapp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace wcapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks for your salary
+            Console.Write("Ka du tjen i året? ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+            // Add error handling if not int here.
+
+            // Asks for how long you were on the can
+            Console.Write("Kor lenge satt du på dass? ");
+            int wctime = Convert.ToInt32(Console.ReadLine());
+
+            // Add error handling if not int here.
+
+            // calculates how much you earned while sitting on the can
+            int shitMoney = ((salary / 1850) / 60 * wctime);
+            Console.WriteLine();
+            Console.WriteLine("Du tjente " + (shitMoney) + " krona mens du va på dass");
+
+        }
+    }
+}
