8f6e6ef73518a9eaf6448592d3e3d408ab635ae3
masunile <helge@furion.se>
2019-10-05 02:10:17 +0200

    Started on a JS version

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Take input from salary
+const salary = window.prompt();
+
+// Take input from timespent
+const timeSpent = window.prompt();
+
+// Calculate how much money you earned taking a shit
+// ## ((salary / 1850) / 60 * timeSpent)
+
+
